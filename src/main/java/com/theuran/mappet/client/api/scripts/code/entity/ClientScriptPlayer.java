201ac2b20199639c77bac3ba23297b9901f56419
llama_orp <jenya-vev@list.ru>
2025-10-29 10:42:56 +0300

    Completed the events Reworked the script launch Added a method for launching a server script from the client

--- src/main/java/com/theuran/mappet/client/api/scripts/code/entity/ClientScriptPlayer.java
+++ src/main/java/com/theuran/mappet/client/api/scripts/code/entity/ClientScriptPlayer.java
@@ -1,6 +1,8 @@
 package com.theuran.mappet.client.api.scripts.code.entity;
 
 import com.theuran.mappet.client.api.scripts.code.ClientScriptOptions;
+import com.theuran.mappet.network.Dispatcher;
+import com.theuran.mappet.network.packets.server.RunScriptPacket;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.text.Text;
@@ -15,10 +17,14 @@ public class ClientScriptPlayer extends ClientScriptEntity<ClientPlayerEntity> {
     }
 
     public void send(String message) {
-        this.entity.sendMessage(Text.of(MinecraftClient.getInstance().options.getMainArm().getValue().asString()));
+        this.entity.sendMessage(Text.of(message));
     }
 
     public ClientScriptOptions getOptions() {
         return new ClientScriptOptions();
+    }
+
+    public void executeServerScript(String name, String function) {
+        Dispatcher.sendToServer(new RunScriptPacket(name, function));
     }
 }
