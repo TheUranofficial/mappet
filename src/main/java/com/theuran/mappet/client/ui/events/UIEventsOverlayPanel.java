cea99779785fd65746be75f5d701f0e0ff281e1c
TheUran <xb7995699@gmail.com>
2025-11-09 19:59:20 +0200

    Textures

--- src/main/java/com/theuran/mappet/client/ui/events/UIEventsOverlayPanel.java
+++ src/main/java/com/theuran/mappet/client/ui/events/UIEventsOverlayPanel.java
@@ -1,10 +1,41 @@
 package com.theuran.mappet.client.ui.events;
 
+import com.theuran.mappet.api.events.EventType;
 import com.theuran.mappet.client.ui.UIMappetKeys;
+import mchorse.bbs_mod.l10n.keys.IKey;
+import mchorse.bbs_mod.ui.framework.elements.input.list.UILabelList;
+import mchorse.bbs_mod.ui.framework.elements.input.list.UIList;
+import mchorse.bbs_mod.ui.framework.elements.input.list.UIStringList;
+import mchorse.bbs_mod.ui.framework.elements.overlay.UIOverlay;
 import mchorse.bbs_mod.ui.framework.elements.overlay.UIOverlayPanel;
+import mchorse.bbs_mod.ui.framework.elements.overlay.UIStringOverlayPanel;
+import mchorse.bbs_mod.ui.utils.Label;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UIEventsOverlayPanel extends UIOverlayPanel {
+    public UILabelList<String> list;
+
+    private String latest = "";
+
     public UIEventsOverlayPanel() {
         super(UIMappetKeys.EVENTS_TITLE);
+
+        this.list = new UILabelList<>(strings -> {
+            for (Label<String> string : strings) {
+                if (this.latest.equals(string.value)) {
+
+                }
+                this.latest = string.value;
+            }
+        });
+        this.list.full(this.content);
+        this.content.add(this.list);
+
+        for (EventType value : EventType.values()) {
+            this.list.add(value.getName(), value.name().toLowerCase());
+        }
     }
 }
